6596b87d5d1999accde31c508bb4f1c268b40dc9
PaoloLongato <paolo.longato@gmail.com>
2015-07-25 11:30:17 +0200

    Initial Commit

---
+++ tagger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tagger
+//
+//  Created by Paolo Longato on 25/07/2015.
+//  Copyright (c) 2015 Paolo Longato. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
